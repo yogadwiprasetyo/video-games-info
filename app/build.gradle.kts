3a162536dfcddd69e5927ac5d22821148e422946
yogadwiprasetyo <jobsforyoga@gmail.com>
2023-10-19 21:13:25 +0700

    refactor(gradle): upgrade targetSdk latest version (34)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "technical.test.yprsty"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "technical.test.yprsty"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
