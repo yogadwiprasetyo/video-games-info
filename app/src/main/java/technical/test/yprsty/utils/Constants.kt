71714397a77d2e3d2eb28a3fb94e8b01f1cf60ea
yogadwiprasetyo <jobsforyoga@gmail.com>
2023-10-21 13:51:28 +0700

    feat(utility): add utilities code or resource

---
+++ app/src/main/java/technical/test/yprsty/utils/Constants.kt
@@ -0,0 +1,5 @@
+package technical.test.yprsty.utils
+
+import technical.test.yprsty.BuildConfig
+
+val apiKey = BuildConfig.API_KEY
--- app/src/main/java/technical/test/yprsty/utils/extension/Extensions.kt
+++ app/src/main/java/technical/test/yprsty/utils/extension/Extensions.kt
@@ -1,2 +1,20 @@
 package technical.test.yprsty.utils.extension
 
+import android.widget.ImageView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+fun <T: RecyclerView.Adapter<*>> RecyclerView.setup(
+    adapterView: T,
+    customLayoutManager: RecyclerView.LayoutManager = LinearLayoutManager(this.context)
+) {
+    layoutManager = customLayoutManager
+    adapter = adapterView
+}
+
+fun ImageView.loadImage(source: Any) {
+    Glide.with(this.context)
+        .load(source)
+        .into(this)
+}
